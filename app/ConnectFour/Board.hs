de5588a6fc9244222b76df65527962b9a6ccf2be
Luc Zwanenberg <luc.zwanenberg@kabisa.nl>
2020-01-28 16:35:56 +0100

    Improve board UI

--- app/ConnectFour/Board.hs
+++ app/ConnectFour/Board.hs
@@ -12,13 +12,13 @@ discForPlayer :: Player -> Disc
 discForPlayer PlayerOne = Yellow
 discForPlayer PlayerTwo = Red
 
-header = " A   B   C   D   E   F   G\n"
+header = " A  B  C  D  E  F  G\n"
 
 showSlot :: Slot -> [Char]
 showSlot Nothing = "[ ]"
 showSlot (Just Yellow) = "[X]"
 showSlot (Just Red) = "[O]"
-showRow (a0, a1, a2, a3, a4, a5, a6) = (showSlot a0) ++ " " ++ (showSlot a1) ++ " " ++ (showSlot a2) ++ " " ++ (showSlot a3) ++ " " ++ (showSlot a4) ++ " " ++ (showSlot a5) ++ " " ++ (showSlot a6)
+showRow (a0, a1, a2, a3, a4, a5, a6) = (showSlot a0) ++ "" ++ (showSlot a1) ++ "" ++ (showSlot a2) ++ "" ++ (showSlot a3) ++ "" ++ (showSlot a4) ++ "" ++ (showSlot a5) ++ "" ++ (showSlot a6)
 showBoard (a0, a1, a2, a3, a4, a5) = header ++ (showRow a0) ++ "\n" ++ (showRow a1) ++ "\n" ++ (showRow a2) ++ "\n" ++ (showRow a3) ++ "\n" ++ (showRow a4) ++ "\n" ++ (showRow a5)
 
 type ColumnNumber = Int
